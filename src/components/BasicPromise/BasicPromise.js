74bce8bafcc35771525bc99346b37b9b8f92a7c7
Kameron <kamahler@gmail.com>
2020-11-28 00:35:50 -0700

    handle status and catch

--- src/components/BasicPromise/BasicPromise.js
+++ src/components/BasicPromise/BasicPromise.js
@@ -1,43 +1,51 @@
 import React, { useState, useEffect } from 'react'
 
 export default function BasicPromise() {
-  const [users, setUsers] = useState([])
-
+  const [users, setUsers] = useState(null)
+  const [error, setError] = useState(null)
   const endpointRandom =
-    Math.random() > 0.5 ? '' : 'http://jsonplaceholder.typicode.com/users'
-
-  const catchError = rej => {
-    console.log(rej)
-  }
+    Math.random() > 0.5
+      ? 'http://jsonplaceholder.typicode.com/userss'
+      : 'http://jsonplaceholder.typicode.com/users'
 
   const apiRequest = endpoint => {
     fetch(endpoint)
       .then(res => {
         if (!res.ok) {
-          console.error(res.status)
+          setError(res.statusText)
           return
+        } else {
+          res.json().then(json => {
+            setUsers(json)
+          })
         }
-        console.log(res.status)
-
-        res.json().then(json => {
-          setUsers(json)
-        })
       })
-      .catch(rej => catchError(rej))
+      .catch(rej => {
+        console.error(JSON.stringify(rej.toString()))
+      })
   }
 
   useEffect(() => {
     apiRequest(endpointRandom)
   }, [])
 
-  return (
-    <>
-      <h1>Fetch, promise</h1>
-      <ul>
-        {users.map(user => {
-          return <li key={user.id}>{user.name}</li>
-        })}
-      </ul>
-    </>
-  )
+  if (users) {
+    return (
+      <>
+        <h1>Fetch, promise</h1>
+        <ul>
+          {users.map(user => {
+            return <li key={user.id}>{user.name}</li>
+          })}
+        </ul>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h1>Fetch, promise</h1>
+        <p>{error}</p>
+      </>
+    )
+  }
 }
